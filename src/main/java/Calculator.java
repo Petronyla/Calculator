c9c78f5976cc0ed1e430d164ba78eca915ad9887
Petronyla <kubickova.petka@gmail.com>
2020-07-01 22:50:19 +0200

    Multi digit numbers

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -23,49 +23,67 @@ public class Calculator {
             operators.put(")", 4);
 
             List<String> list = new ArrayList<>(Arrays.asList(input.split("")));
+            StringBuilder sb = new StringBuilder();
 
-            do {
-                for (int i = 0; list.size() > i; i++) {
-                    if (operators.containsKey(list.get(i))) {
-                        stackOp.push(list.get(i));
-                    } else {
-                        try {
-                            stackNum.push(parseInt(list.get(i)));
-                        } catch (Exception e) {
-                            System.out.println("Unexpected character.");
+            for (int i = 0; list.size() > i; i++) {
+//                if ((list.get(0).equals("-")) || i > 0 && list.get(i).equals("-") && operators.containsKey(list.get(i - 1))) {
+//                    StringBuilder sb = new StringBuilder();
+//                    sb.append("-");
+//                }
+                if (operators.containsKey(list.get(i))) {
+                    stackOp.push(list.get(i));
+                } else {
+
+                    //treats a situation where there is a multi-digit number in the expression
+                    sb.append(input.charAt(i));
+                    for (int j = i + 1; j < list.size(); j++) {
+                        char ch = input.charAt(j);
+                        if (Character.isDigit(ch)) {
+                            sb.append(ch);
+                            i++;
+                        } else {
+                            break;
                         }
                     }
-                    if (stackOp.size() >= 1 && stackNum.size() >= 2) {
-                        if (stackOp.size() >= 2) {
-                            String lastOp = stackOp.pollFirst();
-                            String secondLastOp = stackOp.pollFirst();
 
-                            //calculates the expression in parentheses
-                            if (lastOp.equals(")")) {
-                                do {
-                                    stackNum.push(calculate(secondLastOp));
-                                } while (!stackOp.getFirst().equals("("));
-                                stackOp.pollFirst();
-
-                                //compares the two operators and, if the first operator has the higher priority, performs the
-                                // calculation
-                            } else if (operators.get(secondLastOp) >= operators.get(lastOp) && !secondLastOp.equals("(")) {
-                                stackNum.push(calculate(secondLastOp));
-                                stackOp.push(lastOp);
-
-                                //continues unchanged
-                            } else {
-                                stackOp.push(secondLastOp);
-                                stackOp.push(lastOp);
-                            }
-                        }
-                        if (i == list.size() - 1) {
-                            while (stackOp.size() > 0)
-                                stackNum.push(calculate(stackOp.pollFirst()));
-                        }
+                    try {
+                        stackNum.push(Integer.parseInt(sb.toString()));
+                        sb.setLength(0);
+                    } catch (Exception e) {
+                        System.out.println("Unexpected character.");
                     }
                 }
-            } while (!(stackOp.size() == 0));
+
+                if (stackOp.size() >= 1 && stackNum.size() >= 2) {
+                    if (stackOp.size() >= 2) {
+                        String lastOp = stackOp.pollFirst();
+                        String secondLastOp = stackOp.pollFirst();
+
+                        //calculates the expression in parentheses
+                        if (lastOp.equals(")")) {
+                            do {
+                                stackNum.push(calculate(secondLastOp));
+                            } while (!stackOp.getFirst().equals("("));
+                            stackOp.pollFirst();
+
+                            //compares the two operators and, if the first operator has the higher priority, performs the
+                            // calculation
+                        } else if (operators.get(secondLastOp) >= operators.get(lastOp) && !secondLastOp.equals("(")) {
+                            stackNum.push(calculate(secondLastOp));
+                            stackOp.push(lastOp);
+
+                            //continues unchanged
+                        } else {
+                            stackOp.push(secondLastOp);
+                            stackOp.push(lastOp);
+                        }
+                    }
+                    if (i == list.size() - 1) {
+                        while (stackOp.size() > 0)
+                            stackNum.push(calculate(stackOp.pollFirst()));
+                    }
+                }
+            }
 
             System.out.print("The result is: ");
             System.out.println(stackNum.poll());
